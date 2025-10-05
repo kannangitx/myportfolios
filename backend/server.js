e806fdaf7f4289edfff517d7a1f78584d4bbf145
kannangitx <kannanmr984@gmail.com>
2025-10-06 00:27:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,10 @@ const __dirname = path.dirname(__filename);
 
 // ✅ Serve React build
 app.use(express.static(path.join(__dirname, "../build")));
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
